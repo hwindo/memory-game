641c2888fc905d7183fa27b523e84fd425cdb67e
windo <herwindo.artono@go-jek.com>
2020-05-30 21:25:56 +0700

    feat: add launch screen

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,59 @@
 <template>
   <div id="app">
-    <div class="app-container container">
-      <div class="row w-100 text-left">
-        <div class="col">
-          <div class="score">
-            Score: <span class="score__number">{{ score }}</span>
+    <template v-if="launchScreen">
+      <LaunchScreen></LaunchScreen>
+    </template>
+    <template v-else>
+      <div class="app-container container">
+        <div class="row w-100 text-left">
+          <div class="col">
+            <div class="score">
+              Score: <span class="score__number">{{ score }}</span>
+            </div>
           </div>
         </div>
+        <Squares
+          :score="score"
+          :mode="mode"
+          v-on:answer-right="handleAnswerRight"
+          v-on:answer-wrong="handleAnswerWrong"
+        ></Squares>
+        <Actions
+          :mode="mode"
+          v-on:next="handleNext"
+          v-on:answer-right="handleAnswerRight"
+          v-on:answer-wrong="handleAnswerWrong"
+        ></Actions>
       </div>
-      <Squares
-        :score="score"
-        :mode="mode"
-        v-on:answer-right="handleAnswerRight"
-        v-on:answer-wrong="handleAnswerWrong"
-      ></Squares>
-      <Actions
-        :mode="mode"
-        v-on:next="handleNext"
-        v-on:answer-right="handleAnswerRight"
-        v-on:answer-wrong="handleAnswerWrong"
-      ></Actions>
-    </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import LaunchScreen from "./components/LaunchScreen.vue";
 import Squares from "./components/Squares.vue";
 import Actions from "./components/Actions.vue";
 import { Mode } from "./models/mode";
 
 @Component({
   components: {
+    LaunchScreen,
     Squares,
-    Actions
-  }
+    Actions,
+  },
 })
 export default class App extends Vue {
   mode = Mode.revealActives;
   appState = Mode;
   score = 0;
+  launchScreen = true;
+
+  mounted() {
+    setTimeout(() => {
+      this.launchScreen = false;
+    }, 1618);
+  }
 
   handleNext(): void {
     switch (this.mode) {
@@ -91,9 +105,20 @@ body * {
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  animation: zoom 0.618s forwards;
 }
 
 .score {
   font-size: 2rem;
+}
+@keyframes zoom {
+  0% {
+    opacity: 0;
+    transform: scale(1.618);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
 }
 </style>
---
+++ src/components/LaunchScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="launch d-flex justify-content-center align-items-center">
+    <h1>Memory-Game</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.launch {
+  width: 100vw;
+  height: 100vh;
+  opacity: 0;
+  animation: fade-in 1.618s forwards;
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
